8839f406f0402fc2fdc1f9272471af2bd404de72
Ramon Dina <ramon.z.dina@gmail.com>
2022-01-20 16:15:18 +0100

    split vars into different files.

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 module "webapp" {
   source = "./modules/webapp"
 
-  image_tag   = lookup(var.image_tag, local.env)
-  expose_port = lookup(var.expose_port, local.env)
-  color       = lookup(var.color, local.env)
-}
-
-# testing
-output "environment" {
-  value = lookup(var.env, local.env)
+  image_tag   = var.image_tag
+  expose_port = var.expose_port
+  color       = var.color
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,5 @@
-locals {
-  env = terraform.workspace
-}
+variable "image_tag" {}
 
-variable "image_tag" {
-  type = map(any)
-  default = {
-    stage = "latest"
-    live  = "latest"
-  }
-}
+variable "expose_port" {}
 
-variable "expose_port" {
-  type = map(any)
-  default = {
-    stage = 8080
-    live  = 80
-  }
-}
-
-variable "color" {
-  type = map(any)
-  default = {
-    stage = "red"
-    live  = "green"
-  }
-}
-
-variable "env" {
-  type = map(any)
-  default = {
-    stage = "Hello from stage"
-    live  = "Hello from live"
-  }
-}
+variable "color" {}
